9f0d7fe417ef0b2474ee8eac9b4f04dca74266b0
wangyq25 <wangyq25@asiainfo.com>
2022-11-19 23:39:45 +0800

    finished 80/94

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -8,7 +8,6 @@
 
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
@@ -32,7 +31,10 @@ fn parse_pos_nonzero(s: &str)
 {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
+    let x: i64 = match s.parse() {
+        Ok(x) => x,
+        Err(e) => { return Err(ParsePosNonzeroError::ParseInt(e)); }
+    };
     PositiveNonzeroInteger::new(x)
         .map_err(ParsePosNonzeroError::from_creation)
 }
